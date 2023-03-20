752d5b3640ea6f917912685fd38a26595d185de4
Colin Zhu <colinjzhu@gmail.com>
2023-03-20 09:25:40 -0400

    data + changed ma file

--- moving-average.py
+++ moving-average.py
@@ -43,25 +43,48 @@ class Trader:
 
         window_size = 10
 
-        ma = MovingAverage(window_size)
+        moving_average = MovingAverage(window_size)
 
         # Iterate over all the keys (the available products) contained in the order depths
         for product in state.order_depths.keys():
 
-            # Check if the current product is the 'PEARLS' product, only then run the order logic
-            if product == 'PEARLS':
+            if product == 'BANANAS':
 
                 # Retrieve the Order Depth containing all the market BUY and SELL orders for PEARLS
                 order_depth: OrderDepth = state.order_depths[product]
 
                 # Initialize the list of Orders to be sent as an empty list
                 orders: list[Order] = []
+                
+                mid_price = self.get_mid_price(state, product)
+                ma = moving_average.next(mid_price)
 
-                if len(order_depth.sell_orders) > 0:
 
 
 
         return results
+    
+    def get_mid_price(state, product):
+        order_depth: OrderDepth = state.order_depths[product]
+
+        total_ask_qty = 0
+        total_ask_price = 0
+
+        for ask_price in order_depth.sell_orders.keys():
+            ask_qty = order_depth.sell_orders[ask_price]
+            total_ask_qty += ask_qty
+            total_ask_price += ask_price * ask_qty    
+
+        total_bid_qty = 0
+        total_bid_price = 0
+
+        for bid_price in order_depth.buy_orders.keys():
+            bid_qty = order_depth.buy_orders[bid_price]
+            total_bid_qty += bid_qty
+            total_bid_price += bid_price * bid_qty
+       
+        return (total_ask_price + total_bid_price) / (total_ask_qty + total_bid_qty)
+        
     
   
         
