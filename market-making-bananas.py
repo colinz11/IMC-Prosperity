d5e9f0162e5209bb1e41f28353d8f9967371b2ce
Colin Zhu <colinjzhu@gmail.com>
2023-03-21 00:37:39 -0400

    mm bananas

---
+++ market-making-bananas.py
@@ -0,0 +1,42 @@
+from datamodel import *
+
+class Trader:
+    def __init__(self) -> None:
+        self.sell = 0
+        self.buy = 0
+    def run(self, state: TradingState) -> Dict[str, List[Order]]:
+        result = {}
+        orders: list[Order] = []
+        product = 'BANANAS'
+
+        position = 0 
+        if product in state.position.keys():
+            position = state.position[product]
+           
+        max_sell = -abs(position-20)
+        max_buy = abs(position-20)
+
+        best_ask = min(state.order_depths[product].sell_orders.keys()) 
+        best_bid = max(state.order_depths[product].buy_orders.keys()) 
+        mid_price = (best_ask + best_bid) / 2
+        print('mid_pirce: ' + str(mid_price))
+
+
+        if position == 0: 
+            orders.append(Order(product, mid_price + 2, max_sell))
+            orders.append(Order(product, mid_price - 2, max_buy))
+        if position >= 0: 
+
+        
+        result[product] = orders
+        print(state.own_trades)
+        print(state.position)
+        if product in state.own_trades.keys():
+            for trade in state.own_trades[product]:
+                if trade.timestamp == state.timestamp - 100:
+                    if trade.buyer == 'SUBMISSION':
+                        self.buy += trade.price*trade.quantity
+                    else:
+                        self.sell += trade.price*trade.quantity
+        print(self.sell - self.buy)
+        return result
--- moving-average.py
+++ moving-average.py
@@ -64,13 +64,17 @@ class MovingAverage:
         
 class Trader:
     
+    def __init__(self) -> None:
+        self.ma20 = MovingAverage(20)
+        self.ma50 = MovingAverage(50)
+        self.sell = 0
+        self.buy = 0
 
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
         
         results = {}
 
-        moving_average = MovingAverage(10)
-
+       
         # Iterate over all the keys (the available products) contained in the order depths
         for product in state.order_depths.keys():
 
@@ -81,16 +85,43 @@ class Trader:
 
                 # Initialize the list of Orders to be sent as an empty list
                 orders: list[Order] = []
+                best_ask = min(state.order_depths[product].sell_orders.keys()) 
+                best_bid = max(state.order_depths[product].buy_orders.keys()) 
+                mid_price = (best_ask + best_bid) / 2
+                print('mid price ' + str(mid_price))
+                ma_20 = self.ma20.next(mid_price)
+                ma_50 = self.ma50.next(mid_price)
                 
-                mid_price = self.get_mid_price(state, product)
-                ma = moving_average.next(mid_price)
+              
+                if ma_20 > ma_50: #we buy
+                    if len(order_depth.sell_orders) > 0:
+                        best_ask = min(order_depth.sell_orders.keys())
+                        best_ask_volume = order_depth.sell_orders[best_ask]
+                        print("BUY", str(-best_ask_volume) + "x", best_ask)
+                        orders.append(Order(product, best_ask, -best_ask_volume))
+                    
+                if ma_20 < ma_50: #we sell
+                    if len(order_depth.buy_orders) > 0:
+                        best_bid = max(order_depth.buy_orders.keys())
+                        best_bid_volume = order_depth.buy_orders[best_bid]
+                        print("SELL", str(best_bid_volume) + "x", best_bid)
+                        orders.append(Order(product, best_bid, -best_bid_volume))
 
+                results[product] = orders
 
-
-
+                print(state.own_trades)
+                print(state.position)
+                if product in state.own_trades.keys():
+                    for trade in state.own_trades[product]:
+                        if trade.timestamp == state.timestamp - 100:
+                            if trade.buyer == 'SUBMISSION':
+                                self.buy += trade.price*trade.quantity
+                            else:
+                                self.sell += trade.price*trade.quantity
+                print(self.sell - self.buy)
         return results
     
-    def get_mid_price(state, product):
+    def get_mid_price(self, state, product):
         order_depth: OrderDepth = state.order_depths[product]
 
         total_ask_qty = 0
@@ -109,6 +140,8 @@ class Trader:
             total_bid_qty += bid_qty
             total_bid_price += bid_price * bid_qty
        
+        if (total_ask_qty + total_bid_qty) == 0:
+            return 0
         return (total_ask_price + total_bid_price) / (total_ask_qty + total_bid_qty)
         
     
