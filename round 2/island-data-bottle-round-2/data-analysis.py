330f7e9361ede5ef1a4e33b8734a400a7d8fc9e0
Colin Wang <77744524+cjiwang@users.noreply.github.com>
2023-03-23 23:37:32 -0400

    data analysis on coco/pc

--- round 2/island-data-bottle-round-2/data-analysis.py
+++ round 2/island-data-bottle-round-2/data-analysis.py
@@ -2,6 +2,8 @@ import math
 from collections import deque
 
 import pandas as pd
+import statsmodels.api as stat
+import statsmodels.tsa.stattools as ts
 import numpy as np
 from math import sqrt
 
@@ -32,28 +34,57 @@ def get_average(b1, b2, b3):
 
 
 def main():
-    fileName = "prices_round_2_day_-1.csv"
-    product = "COCONUTS"
+    fileName = "prices_round_2_day_0.csv"
+    # product = "COCONUTS"
 
-    df = read_market_data(fileName)
+    df_coco = read_market_data(fileName)
+    df_pc = read_market_data(fileName)
     # print(df['product'])
-    df.drop(df.loc[df['product'] != product].index, inplace=True)
-    df = df.replace('', 0)
+    df_coco.drop(df_coco.loc[df_coco['product'] != "COCONUTS"].index, inplace=True)
+    df_pc.drop(df_pc.loc[df_pc['product'] != "PINA_COLADAS"].index, inplace=True)
 
-    bidSum = 0
-    askSum = 0
+    df_coco = df_coco.replace('', 0)
+    df_pc = df_pc.replace('', 0)
 
-    for i in range(len(df.index)):
-        bidSpread = get_average(float(df.iloc[i]['bid_price_1']), float(df.iloc[i]['bid_price_2']),
-                                float(df.iloc[i]['bid_price_3'])) - float(df.iloc[i]['mid_price'])
-        bidSum += bidSpread
-    print(f"Average mid-bid minus mid-price {bidSum / len(df.index)}")
+    # for i in range(len(df_coco.index)):
+    #     print(10 * (1.9 * math.log(df_coco.iloc[i]['mid_price']) - math.log(df_pc.iloc[i]['mid_price'])))
 
-    for i in range(len(df.index)):
-        askSpread = get_average(float(df.iloc[i]['ask_price_1']), float(df.iloc[i]['ask_price_2']),
-                                float(df.iloc[i]['ask_price_3'])) - float(df.iloc[i]['mid_price'])
-        askSum += askSpread
-    print(f"Average mid-ask minus mid-price {askSum / len(df.index)}")
+
+    df = pd.DataFrame()
+
+    df_coco.rename(columns={'mid_price': 'coco_price'}, inplace=True)
+    df_pc.rename(columns={'mid_price': 'pc_price'}, inplace=True)
+    coco_price = df_coco['coco_price']
+    pc_price = df_pc['pc_price']
+
+    df['coco_price'] = list(coco_price)
+
+    df['pc_price'] = list(pc_price)
+
+    print(df)
+
+    result = stat.OLS(df['coco_price'], df['pc_price']).fit()
+    c_t = ts.adfuller(result.resid)
+
+    if c_t[0] <= c_t[4]['10%'] and c_t[1] <= 0.1:
+        print("Pair of securities is co-integrated")
+    else:
+        print("Pair of securities is not co-integrated")
+
+    # bidSum = 0
+    # askSum = 0
+    #
+    # for i in range(len(df.index)):
+    #     bidSpread = get_average(float(df.iloc[i]['bid_price_1']), float(df.iloc[i]['bid_price_2']),
+    #                             float(df.iloc[i]['bid_price_3'])) - float(df.iloc[i]['mid_price'])
+    #     bidSum += bidSpread
+    # print(f"Average mid-bid minus mid-price {bidSum / len(df.index)}")
+    #
+    # for i in range(len(df.index)):
+    #     askSpread = get_average(float(df.iloc[i]['ask_price_1']), float(df.iloc[i]['ask_price_2']),
+    #                             float(df.iloc[i]['ask_price_3'])) - float(df.iloc[i]['mid_price'])
+    #     askSum += askSpread
+    # print(f"Average mid-ask minus mid-price {askSum / len(df.index)}")
 
 
 #    totalSum = 0
