9c674f3a1671e356b792f8bc4a9db0ae62fd87ea
louishu17 <lh329@duke.edu>
2023-03-30 00:27:48 -0400

    olivia berries strat

--- round_5/island-data-bottle-round-5/ukelele.py
+++ round_5/island-data-bottle-round-5/ukelele.py
@@ -67,6 +67,10 @@ class Trader:
         self.ema26 = ExponentialMovingAverage(26)
         self.signal = 0
         self.position = 0
+        self.keep_buying_ukuleles = False
+        self.keep_selling_ukuleles = False
+        self.keep_buying_berries = False
+        self.keep_selling_berries = False
 
     def update(self, count):
         self.sightings.append(count)
@@ -83,18 +87,13 @@ class Trader:
 
         result = {}
         orders_ukulele: list[Order] = []
-        keep_buying = False
-        keep_selling = False
+        orders_berries: list[Order] = []
 
         for product in state.order_depths.keys():
             if product == "UKULELE":
-                ukelele_trades = state.market_trades.get("UKULELE", [])
-
                 ukulele_order_depth: OrderDepth = state.order_depths[product]
                 ukulele_best_ask = min(ukulele_order_depth.sell_orders.keys())
-                # ukelele_worst_ask = max(ukulele_order_depth.sell_orders.keys())
                 ukulele_best_bid = max(ukulele_order_depth.buy_orders.keys())
-                # ukelele_worst_bid = min(ukulele_order_depth.buy_orders.keys())
                 ukulele_mid_price = (ukulele_best_ask + ukulele_best_bid) / 2
 
                 if product in state.position.keys():
@@ -105,7 +104,7 @@ class Trader:
                 ukulele_buy = 70 - ukulele_position
                 ukulele_sell = 70 - (-1 * ukulele_position)
 
-                # print(len(ukelele_trades))
+                ukelele_trades = state.market_trades.get("UKULELE", [])
 
                 for trade in ukelele_trades:
                     if trade.timestamp == state.timestamp - 100:
@@ -115,8 +114,8 @@ class Trader:
                             # print("OLIVIA BUY PRICE: " + str(trade.price))
                             # print("UKELELE MID PRICE: " +
                             #       str(ukulele_mid_price))
-                            keep_buying = True
-                            keep_selling = False
+                            self.keep_buying_ukuleles = True
+                            self.keep_selling_ukuleles = False
                         if trade.seller == "Olivia":
                             # print(state.timestamp)
                             # print("OLIVIA SELL")
@@ -125,17 +124,79 @@ class Trader:
                             #       str(ukulele_mid_price))
                             # orders_ukulele.append(
                             #     Order("UKULELE", ukulele_mid_price-1, -ukulele_sell))
-                            keep_selling = True
-                            keep_buying = False
+                            self.keep_selling_ukuleles = True
+                            self.keep_buying_ukuleles = False
 
-        if keep_buying:
+            if product == "BERRIES":
+                sell_timestamp = 350000
+                buy_timestamp = 500000
+                close = 700000
+                order_depth: OrderDepth = state.order_depths[product]
+                orders: list[Order] = []
+
+                if product in state.position.keys():
+                    current_position = state.position[product]
+                else:
+                    current_position = 0
+
+                berries_buy = 250 - current_position
+                berries_sell = 250 - (-1 * current_position)
+
+                best_ask = min(order_depth.sell_orders.keys())
+                best_bid = max(order_depth.buy_orders.keys())
+                berries_mid_price = (best_ask + best_bid) / 2
+
+                sell_price = best_ask - 1
+                bid_price = best_bid + 1
+
+                if sell_price < bid_price:
+                    sell_price = berries_mid_price
+                    bid_price = berries_mid_price
+
+                berries_trades = state.market_trades.get("BERRIES", [])
+
+                for trade in berries_trades:
+                    if trade.timestamp == state.timestamp - 100:
+                        if trade.buyer == "Olivia":
+                            self.keep_buying_berries = True
+                            self.keep_selling_berries = False
+                        if trade.seller == "Olivia":
+                            self.keep_selling_berries = True
+                            self.keep_buying_berries = False
+
+                if self.keep_buying_berries is False and self.keep_selling_berries is False:
+                    if state.timestamp >= sell_timestamp and state.timestamp < buy_timestamp:  # buy
+                        orders_berries.append(
+                            Order(product, best_ask, berries_buy))
+                    elif state.timestamp >= buy_timestamp and state.timestamp < close:  # sell
+                        orders_berries.append(
+                            Order(product, best_bid, -berries_sell))
+                    elif state.timestamp == close:  # buy
+                        orders_berries.append(
+                            Order(product, best_ask, current_position))
+                    else:  # market make
+                        orders_berries.append(
+                            Order(product, sell_price, -berries_sell))
+                        orders_berries.append(
+                            Order(product, bid_price, berries_buy))
+
+        if self.keep_buying_ukuleles:
             orders_ukulele.append(
                 Order("UKULELE", ukulele_mid_price + 1, ukulele_buy))
 
-        if keep_selling:
+        if self.keep_selling_ukuleles:
             orders_ukulele.append(
                 Order("UKULELE", ukulele_mid_price - 1, -ukulele_sell))
 
-        result['UKULELE'] = orders_ukulele
-        #logger.flush(state, result)
+        if self.keep_buying_berries:
+            orders_berries.append(
+                Order("BERRIES", berries_mid_price+1, berries_buy))
+        if self.keep_selling_berries:
+            orders_berries.append(
+                Order("BERRIES", berries_mid_price-1, -berries_sell))
+
+        # result['UKULELE'] = orders_ukulele
+        result["BERRIES"] = orders_berries
+
+        # logger.flush(state, result)
         return result
